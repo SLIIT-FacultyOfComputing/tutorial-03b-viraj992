907ed5f557bf3c297c1a8cb84c628603400bf0e6
viraj992 <99419786+viraj992@users.noreply.github.com>
2022-02-25 12:26:33 +0000

    save

--- exercise02.cpp
+++ exercise02.cpp
@@ -30,4 +30,8 @@ int main() {
     return 0;
 }
 
-// Implement the Volume() function here
+// Implement the Volume() function 
+int volume(int height, int width, int length)
+{
+  return height * width * length;
+}
--- exercise03.cpp
+++ exercise03.cpp
@@ -5,35 +5,42 @@ using namespace std;
 int volume(int height, int width, int length);
 
 // 1. Define a structure called Box
+struct box
+{
+    int height;
+    int width;
+    int length;
+};
 // have the integer data types Height, Width, Length
 
 // Do not change the main function
 int main() {
     // 2. Create a variable called box1 of the Box structure type
+    struct box box1; 
     // int box1Height, box1Width, box1Length;
     // 3. Create a variable called box2 of the Box structure type
-    
+    struct box box2; 
     // int box2Height, box2Width, box2Length;
     int totalVolume;
     
     // 4. Input the height, width, lenght of box1 and box2
     cout << "Enter Box 1 Height : ";
-    cin >> Height of Box 1;
+    cin >> box1.height;
     cout << "Enter Box 1 Width : ";
-    cin >> Width of Box 1;
+    cin >> box1.width;
     cout << "Enter Box 1 Length : ";
-    cin >> Length of Box 1;
+    cin >> box1.length;
     
     cout << "Enter Box 2 Height : ";
-    cin >> Height of Box 2;
+    cin >> box2.height;
     cout << "Enter Box 2 Width : ";
-    cin >> Width of Box 2;
+    cin >> box2.width;
     cout << "Enter Box 2 Length : ";
-    cin >> Length of Box 2;
+    cin >> box2.length;
     
     // 5. Replace the coding below to pass the Box type structure
-    totalVolume = volume( , , )
-             + volume( , , );
+    totalVolume = volume( box1.height, box1.width, box1.length)
+             + volume( box2.height, box2.width, box2.length);
     
     cout << "Volume of Box is " << totalVolume << endl;
     
@@ -41,3 +48,7 @@ int main() {
 }
 
 // Implement the functions here
+int volume(int height, int width, int length)
+{
+  return height * width * length;
+}
